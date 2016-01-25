637b4c6d9aa5f711cda7c43668d467ca9228dcc2
SangwookLee <8go4go@gmail.com>
2016-01-25 14:52:51 +0900

    Init Files

---
+++ ObjectOrientied/src/www/kailee/com/FileCopy.java
@@ -0,0 +1,5 @@
+package www.kailee.com;
+
+public class FileCopy {
+
+}
---
+++ ObjectOrientied/src/www/kailee/com/Main.java
@@ -0,0 +1,10 @@
+package www.kailee.com;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
